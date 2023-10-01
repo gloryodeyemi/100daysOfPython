b2125b8e794fe35444db33b22e1ed0816641c799
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-10-01 06:40:07 -0400

    Day 16

--- Day16/day_16/main.py
+++ Day16/day_16/main.py
@@ -11,4 +11,11 @@ print(my_screen.canvheight)
 my_screen.exitonclick()
 
 # Python packages
-import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = 'l'
+
+print(table)
