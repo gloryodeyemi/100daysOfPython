f8526d7b2438315e1fa8d4ba4516244a73a0452f
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-10-05 13:12:14 -0400

    Day 17

--- Day17/main.py
+++ Day17/main.py
@@ -1,5 +1,13 @@
 class User:
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
 
 
-user_1 = User()
+user_1 = User("001", "glow")
+print(user_1.id)
+
+
+# PascalCase, camelCase, snake_case
+# attribute is a variable associated with an object
