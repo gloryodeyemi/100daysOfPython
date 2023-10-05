010a9f30858d1ba29f8a9f3a99fa356ec934e4c8
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-10-05 13:53:49 -0400

    Day 17

---
+++ Day17/Quiz_Game/main.py
@@ -0,0 +1,10 @@
+from data import question_data
+from question_model import Question
+
+question_bank = []
+for question_dict in question_data:
+    # print(question_dict['text'])
+    question_bank.append(Question(question_dict['text'], question_dict['answer']))
+
+print(len(question_bank))
+print(question_bank)
--- Day17/main.py
+++ Day17/main.py
@@ -3,11 +3,19 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("001", "glow")
-print(user_1.id)
+user_2 = User("002", "ayorcodes")
 
+user_1.follow(user_2)
+print(f"User 1\n******\nFollowings: {user_1.following}\nFollowers: {user_1.followers}\n")
+print(f"User 2\n******\nFollowings: {user_2.following}\nFollowers: {user_2.followers}")
 
 # PascalCase, camelCase, snake_case
 # attribute is a variable associated with an object
