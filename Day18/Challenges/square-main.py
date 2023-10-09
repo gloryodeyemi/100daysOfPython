5e80c3c09bccf2bb65757d2bcc90294d7eb612ff
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-10-09 19:24:51 -0400

    Day 18

---
+++ Day18/Challenges/square-main.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+square_turtle = Turtle()
+square_turtle.shape("turtle")
+square_turtle.color("DarkViolet")
+
+
+for i in range(4):
+    square_turtle.forward(200)
+    square_turtle.right(90)
+
+# square_turtle.home()
+# clearscreen()
+
+# # algorithmic patterns
+# for steps in range(100):
+#     for c in ('blue', 'red', 'green'):
+#         color(c)
+#         forward(steps)
+#         right(30)
+
+
+screen = Screen()
+screen.exitonclick()
