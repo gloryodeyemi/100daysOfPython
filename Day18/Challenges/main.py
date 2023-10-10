186d7abc98b2708835298203dcf1ee038e36de81
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-10-09 20:16:08 -0400

    Day 18

--- Day18/Challenges/main.py
+++ Day18/Challenges/main.py
@@ -1,9 +1,16 @@
 from turtle import Turtle, Screen
+import random
 
 cal = Turtle()
 cal.shape("turtle")
 cal.color("DarkViolet")
 screen = Screen()
+screen.colormode(255)
+
+
+def random_color():
+    return random.randint(0, 255)
+
 
 # challenge 1 - Draw a square
 for _ in range(4):
@@ -18,6 +25,24 @@ for _ in range(15):
     cal.penup()
     cal.forward(10)
     cal.pendown()
+
+cal.clear()
+cal.penup()
+cal.home()
+cal.pendown()
+
+# challenge 3 - Drawing different shapes
+circle_angle = 360
+shape_sides = 3
+
+for _ in range(10):
+    angle = circle_angle / shape_sides
+    for steps in range(shape_sides):
+        cal.forward(100)
+        cal.right(angle)
+    shape_sides += 1
+    cal.pencolor((random_color(), random_color(), random_color()))  # change cal color
+
 
 # # algorithmic patterns
 # for steps in range(100):
