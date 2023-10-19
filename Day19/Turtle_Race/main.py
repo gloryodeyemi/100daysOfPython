502bfbae0d9bf23fbdbf42ae246fa9da303d0a9c
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-10-19 16:53:28 -0400

    Day 19

--- Day19/Turtle_Race/main.py
+++ Day19/Turtle_Race/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Bet on a turtle", prompt="Which turtle will win the race? (Enter a color): ")
@@ -23,5 +25,20 @@ for color in colors:
     turtles.append(turtle_name)
     y += 30
 
-print(turtles)
+# print(turtles)
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"Congratulations!!! You win. The {winning_color} turtle is the winner.")
+            else:
+                print(f"You lose! The {winning_color} turtle is the winner.")
+
+        turtle.forward(random.randint(0, 10))
+
 screen.exitonclick()
