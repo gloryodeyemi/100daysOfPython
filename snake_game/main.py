c4ea08a458c36a4c8fe7bab34a80f12f64c31749
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-10-20 07:21:58 -0400

    Day 20

--- snake_game/main.py
+++ snake_game/main.py
@@ -31,10 +31,13 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for square_index in range(2, 0, -1):
+    for square_index in range(len(square_snakes) - 1, 0, -1):
         new_x = square_snakes[square_index - 1].xcor()
         new_y = square_snakes[square_index - 1].ycor()
         square_snakes[square_index].goto(new_x, new_y)
+
+    square_snakes[0].forward(20)
+    square_snakes[0].left(90)
 
 
 screen.exitonclick()
