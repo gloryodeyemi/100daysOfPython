96b768317d7bbef75e0e6bf7b1659ec3f9cdc873
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-10-25 08:13:53 -0400

    Day 21

---
+++ Day20_21/scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 280)
+        self.hideturtle()
+        # self.clear()
+
+    def write_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}", move=False, align="center", font=('Phenomena', 15, 'bold'))
+
+    def increment_score(self):
+        self.score += 1
+        self.write_score()
