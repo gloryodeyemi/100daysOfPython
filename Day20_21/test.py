c7f1b7d3f6a3129a2b835e62b487cc56a6b13f64
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-10-25 07:23:47 -0400

    Day 21

---
+++ Day20_21/test.py
@@ -0,0 +1,9 @@
+# Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
+
+# Ask the user for the name of a pet.
+pet = input("What is the name of a pet?\n")
+
+# Combine the name of their city and pet and show them their band name.
+band_name = city + " " + pet
+print("Your band name is: " + band_name)
