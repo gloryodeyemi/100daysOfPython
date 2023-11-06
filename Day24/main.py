72fd7ec8bc7f76ad6c4d10d74deb6cfd71666646
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-11-06 07:02:33 -0500

    day 24

--- Day24/main.py
+++ Day24/main.py
@@ -1,22 +1,26 @@
 # option 1
-file = open("my_file.txt")
+file = open("/Users/new/Downloads/my_file.txt")
 contents = file.read()
 print(contents)
 file.close()
 
 # option 2
-with open("my_file.txt") as file:
+with open("/Users/new/Downloads/my_file.txt") as file:
     contents = file.read()
     print(contents)
 
 # write to a file
-with open("my_file.txt", 'w') as file:
+with open("/Users/new/Downloads/my_file.txt", 'w') as file:
     file.write("I am a new text.")
 
 # add to a file
-with open("my_file.txt", 'a') as file:
+with open("/Users/new/Downloads/my_file.txt", 'a') as file:
     file.write("\nI am another text.")
 
 # create a new file
 with open("new_file.txt", 'w') as file:
     file.write("I am a new file.")
+
+# Absolute file path - starts from the origin
+# Working directory - the directory or folder we are working from
+# Relative file path - starts from the working directory (use ./, ../, etc.)
