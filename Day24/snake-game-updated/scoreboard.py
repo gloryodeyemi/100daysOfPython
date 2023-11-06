47733099a0b8c30aa4cddf6e831f385d2c07a00d
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-11-06 06:22:58 -0500

    day 24

---
+++ Day24/snake-game-updated/scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Phenomena', 15, 'bold')
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 280)
+        self.hideturtle()
+        self.write_score()
+
+    def write_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.write_score()
+
+    def increment_score(self):
+        self.score += 1
+        self.write_score()
