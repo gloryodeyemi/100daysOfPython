68c6db9003b4addb15255e784226cfd77febe417
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-11-09 10:33:40 -0500

    Day 25

--- Day25/main.py
+++ Day25/main.py
@@ -1,5 +1,19 @@
-# data = []
-with open("weather_data.csv") as csv_file:
-    data = csv_file.readlines()
+import csv
+import pandas as pd
 
-print(data)
+# with csv
+with open("weather_data.csv") as csv_file:
+    data = csv.reader(csv_file)
+    temperature = []
+    index = 0
+    for row in data:
+        if index > 0:
+            temperature.append(int(row[1]))
+        print(row)
+        index += 1
+
+print(f"{temperature}\n")
+
+# with pandas
+data = pd.read_csv("weather_data.csv")
+print(data['temp'])
