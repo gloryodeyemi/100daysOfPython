2f4a1e039c44857a6f647652f7896d7170c15b59
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-11-09 12:53:27 -0500

    Day 25

--- Day25/US-states-game/main.py
+++ Day25/US-states-game/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,14 +7,30 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-user_answer = screen.textinput(title="Guess the state", prompt="Enter a state's name")
+# import that data
+states_data = pd.read_csv("50_states.csv")
+states_list = states_data['state'].to_list()
+x_cors = states_data['x'].to_list()
+y_cors = states_data['y'].to_list()
+
+write_turtle = turtle.Turtle()
+write_turtle.penup()
+write_turtle.hideturtle()
+
+score = 0
+correct_guesses = []
+while score < 50:
+    # get user input
+    user_answer = screen.textinput(title=f"{score}/50 Guess the state", prompt="Enter a state's name").title()
+
+    # check if user input is a state in the US
+    if user_answer in states_list and user_answer not in correct_guesses:
+        state_index = states_list.index(user_answer)  # get the index
+        x_cor = x_cors[state_index]  # get the x coordinate
+        y_cor = y_cors[state_index]  # get the y coordinate
+        write_turtle.goto(x_cor, y_cor)  # go to the position
+        write_turtle.write(user_answer)  # write the state
+        score += 1  # increment the score
+        correct_guesses.append(user_answer)  # add user answer to correct guesses list.
 
 screen.exitonclick()
-
-# get x and y coordinates
-# def get_mouse_click_coor(x, y):
-#     print(x, y)
-
-# turtle.onscreenclick(get_mouse_click_coor)
-#
-# turtle.mainloop()
