5f1a4a744b7fdb329c5201bc243d902db791c7c8
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-11-15 13:45:57 -0500

    Day 26

---
+++ Day26/Challenges/data_overlap.py
@@ -0,0 +1,22 @@
+def read_file(file):
+    with open(file) as file:
+        contents = file.readlines()
+    return contents
+
+
+def modify_file_list(file_list):
+    modified_list = [int(num.strip()) for num in file_list]
+    return modified_list
+
+
+file1 = read_file("file1.txt")
+modified_file1 = modify_file_list(file1)
+# print(modified_file1)
+
+file2 = read_file("file2.txt")
+modified_file2 = modify_file_list(file2)
+# print(modified_file2)
+
+result = [num for num in modified_file1 if num in modified_file2]
+# Write your code above 👆
+print(result)
