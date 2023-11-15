2f670b658db97677f29442a473a934f71ebc2dcd
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-11-15 14:13:39 -0500

    Day 26

--- Day26/README.md
+++ Day26/README.md
@@ -13,3 +13,15 @@ new_list = [new_item for item in list]
 * Conditional List Comprehension
 
 ```new_list = [new_item for item in list if test]```
+* Dictionary Comprehension
+
+```
+new_dict = {new_key:new_value for item in list}
+new_dict = {new_key:new_value for (key,value) in dict.items()}
+```
+* Conditional Dictionary Comprehension
+
+```
+new_dict = {new_key:new_value for item in list if test}
+new_dict = {new_key:new_value for (key,value) in dict.items() if test}
+```
--- Day26/main.py
+++ Day26/main.py
@@ -1,22 +1,36 @@
+import random
+
 # list comprehension
+print("List Comprehension\n------------------")
 numbers = [1, 2, 3]
 
 new_list = [n + 1 for n in numbers]
-print(f"Previous list: {numbers}\nNew list: {new_list}\n")
+print(f"Previous list: {numbers}\nNew list: {new_list}")
 
 name = "Glory"
 letters_list = [letter for letter in name]
-print(f"Name list: {letters_list}\n")
+print(f"Name list: {letters_list}")
 
 # Python sequences - list, range, tuple, dictionary.
 
 doubled_numbers = [num * 2 for num in range(1, 5)]
-print(f"Doubled numbers list: {doubled_numbers}\n")
+print(f"Doubled numbers list: {doubled_numbers}")
 
 # conditional list comprehension - new_list = [new_item for item in list if test]
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 short_names = [name for name in names if len(name) <= 4]
-print(f"Short names: {short_names}\n")
+print(f"Short names: {short_names}")
 
 long_names = [name.upper() for name in names if len(name) >= 5]
 print(f"Long names: {long_names}\n")
+
+# dictionary comprehension
+print("Dictionary Comprehension\n------------------------")
+
+score_dict = {name: random.randint(1, 100) for name in names}
+print(f"Student scores: {score_dict}")
+
+passed_students = {student: score for (student, score) in score_dict.items() if score > 50}
+print(f"Passed students: {passed_students}")
+# conditional dictionary comprehension
+
