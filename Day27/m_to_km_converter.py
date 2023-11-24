065f2c44d0a2f131c5117826db8496bc3be1bc60
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2023-11-24 17:54:01 -0500

    Day 27

--- Day27/m_to_km_converter.py
+++ Day27/m_to_km_converter.py
@@ -17,26 +17,26 @@ converter_window.config(padx=50, pady=20)
 # miles entry configuration
 miles_input = Entry(width=10)
 miles_input.insert(END, string="0")
-miles_input.grid(column=2, row=0)
+miles_input.grid(column=1, row=0)
 
 # miles label
 miles_label = Label(text="Miles")
-miles_label.grid(column=3, row=0)
+miles_label.grid(column=2, row=0)
 
 # equal label
 equal_label = Label(text="is equal to")
-equal_label.grid(column=1, row=1)
+equal_label.grid(column=0, row=1)
 
 # km value label
 km_value_label = Label(text="0")
-km_value_label.grid(column=2, row=1)
+km_value_label.grid(column=1, row=1)
 
 # km label
 km_label = Label(text="Km")
-km_label.grid(column=3, row=1)
+km_label.grid(column=2, row=1)
 
 # calculate button
 calc_button = Button(text="Calculate", command=convert)
-calc_button.grid(column=2, row=2)
+calc_button.grid(column=1, row=2)
 
 converter_window.mainloop()
