9955d23b5d2ed987da88a4f2f965716c778ce1cc
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-10 11:43:55 -0500

    Day-28

--- Day28/Pomodoro/main.py
+++ Day28/Pomodoro/main.py
@@ -23,7 +23,12 @@ def reset():
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def countdown(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -37,8 +42,10 @@ timer_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
+
+countdown(5)
 
 start_button = Button(text="Start", command=start, font=(FONT_NAME, 20, "bold"), highlightthickness=0)
 start_button.config(padx=5, pady=5)
