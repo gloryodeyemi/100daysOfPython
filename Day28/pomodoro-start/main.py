446ea871750b9b9cb17b0ff4b5805a2b253de979
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-10 11:13:44 -0500

    Day-28

--- Day28/pomodoro-start/main.py
+++ Day28/pomodoro-start/main.py
@@ -10,7 +10,16 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+def start():
+    pass
+
+
+def reset():
+    pass
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
@@ -19,10 +28,28 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+timer_label.config(padx=10, pady=10)
+timer_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_image)
-canvas.pack()
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", command=start, font=(FONT_NAME, 20, "bold"), highlightthickness=0)
+start_button.config(padx=5, pady=5)
+start_button.grid(column=0, row=2)
+
+check_label = Label(text="✅︎", bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+check_label.config(padx=5, pady=5)
+check_label.grid(column=1, row=3)
+
+reset_button = Button(text="Reset", command=reset, font=(FONT_NAME, 20, "bold"), highlightthickness=0)
+reset_button.config(padx=5, pady=5)
+reset_button.grid(column=2, row=2)
 
 window.mainloop()
