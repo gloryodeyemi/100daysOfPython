53f89be7487e3de35626cff7293ddf86b5a3e8a0
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-10 12:56:55 -0500

    Day-28

--- Day28/Pomodoro/main.py
+++ Day28/Pomodoro/main.py
@@ -10,28 +10,58 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
-def reset():
-    pass
+def reset_timer():
+    global reps
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer", fg=GREEN)
+    check_label.config(text="")
+    reps = 0
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    countdown(int(0.5 * 60))
+    global reps
+    reps += 1
+    work_seconds = WORK_MIN * 60
+    short_break_seconds = SHORT_BREAK_MIN * 60
+    long_break_seconds = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        countdown(int(long_break_seconds))
+        timer_label.config(text="Long break", fg=RED)
+    elif reps % 2 == 0:
+        countdown(int(short_break_seconds))
+        timer_label.config(text="Short break", fg=PINK)
+    else:
+        countdown(int(work_seconds))
+        timer_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def countdown(count):
+    global timer
+
     count_minute = math.floor(count / 60)
     count_seconds = count % 60
-    if count_seconds == 0 or count_seconds < 10:
+    if count_seconds < 10:
         count_seconds = f"0{count_seconds}"
 
     canvas.itemconfig(timer_text, text=f'{count_minute}:{count_seconds}')
     if count > 0:
-        window.after(1000, countdown, count - 1)
+        timer = window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✅︎"
+        check_label.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -53,11 +83,11 @@ start_button = Button(text="Start", command=start_timer, font=(FONT_NAME, 20, "b
 start_button.config(padx=5, pady=5)
 start_button.grid(column=0, row=2)
 
-check_label = Label(text="✅︎", bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+check_label = Label(bg=YELLOW)
 check_label.config(padx=5, pady=5)
 check_label.grid(column=1, row=3)
 
-reset_button = Button(text="Reset", command=reset, font=(FONT_NAME, 20, "bold"), highlightthickness=0)
+reset_button = Button(text="Reset", command=reset_timer, font=(FONT_NAME, 20, "bold"), highlightthickness=0)
 reset_button.config(padx=5, pady=5)
 reset_button.grid(column=2, row=2)
 
