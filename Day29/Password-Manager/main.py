06683270e3ae4cd32e336756822a9ad850f8d02c
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-11 10:25:34 -0500

    Day 29

--- Day29/Password-Manager/main.py
+++ Day29/Password-Manager/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+FONT_NAME = "Salsa"
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -16,30 +18,33 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo_image)
-canvas.grid(column=1, row=0)
+canvas.grid(row=0, column=1)
 
-website_label = Label(text="Website:")
-website_label.grid(column=0, row=1)
+# Labels
+website_label = Label(text="Website:", font=(FONT_NAME, 12, "bold"))
+website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+email_label = Label(text="Email/Username:", font=(FONT_NAME, 12, "bold"))
+email_label.grid(row=2, column=0)
 
-email_label = Label(text="Email/Username:")
-email_label.grid(column=0, row=2)
+password_label = Label(text="Password:", font=(FONT_NAME, 12, "bold"))
+password_label.grid(row=3, column=0)
 
-email_entry = Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2)
+# Entries
+website_entry = Entry(width=37)
+website_entry.grid(row=1, column=1, columnspan=2)
 
-password_label = Label(text="Password:")
-password_label.grid(column=0, row=3)
+email_entry = Entry(width=37)
+email_entry.grid(row=2, column=1, columnspan=2)
 
-password_entry = Entry(width=20)
-password_entry.grid(column=1, row=3)
+password_entry = Entry(width=22)
+password_entry.grid(row=3, column=1)
 
-generate_button = Button(text="Generate Password", command=generate_password, width=11)
-generate_button.grid(column=2, row=3)
+# Buttons
+generate_button = Button(text="Generate Password", command=generate_password, width=11, font=(FONT_NAME, 12, "bold"))
+generate_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", command=generate_password, width=33)
-add_button.grid(column=1, row=4, columnspan=2)
+add_button = Button(text="Add", command=generate_password, width=34, font=(FONT_NAME, 12, "bold"))
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
