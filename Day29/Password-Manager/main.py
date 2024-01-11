a4cf09ad204fa9125275ffaab913269c7dba74ab
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-11 12:10:34 -0500

    Day 29

--- Day29/Password-Manager/main.py
+++ Day29/Password-Manager/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 FONT_NAME = "Salsa"
 LABEL_WIDTH = 15
@@ -8,6 +9,29 @@ LABEL_WIDTH = 15
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     pass
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = [random.choice(letters) for _ in range(nr_letters)]
+password_list += [random.choice(symbols) for _ in range(nr_symbols)]
+password_list += [random.choice(numbers) for _ in range(nr_numbers)]
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -25,6 +49,7 @@ def save():
     url = url_entry.get()
     password = password_entry.get()
 
+    # validation
     if len(website) == 0 or len(url) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showerror(title="Missing details", message="Input field cannot be empty!")
     elif '@' not in email or '.com' not in email:
