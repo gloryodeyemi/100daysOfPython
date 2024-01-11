398b0822969d8b0e647b09d67a741e3f74810626
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-11 10:13:26 -0500

    Day 29

--- Day29/Password-Manager/main.py
+++ Day29/Password-Manager/main.py
@@ -1,6 +1,10 @@
 from tkinter import *
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    pass
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -8,5 +12,34 @@ from tkinter import *
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+logo_image = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+password_entry = Entry(width=20)
+password_entry.grid(column=1, row=3)
+
+generate_button = Button(text="Generate Password", command=generate_password, width=11)
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", command=generate_password, width=33)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
