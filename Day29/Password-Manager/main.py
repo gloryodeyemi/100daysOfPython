a864ad27cd00c5042d480b7a7535e4abf774147a
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-11 09:41:46 -0500

    Day 29

---
+++ Day29/Password-Manager/main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+window.mainloop()
