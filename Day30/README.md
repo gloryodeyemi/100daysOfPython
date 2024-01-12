c567667439e3717924d1d943708d48f379488154
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-12 10:41:40 -0500

    Day 30

---
+++ Day30/README.md
@@ -0,0 +1,5 @@
+## Day 29 - Errors, Exceptions and JSON Data: Improving the Password Manager
+
+**Stage:** Intermediate
+
+**Project:** Password Manager ([try here]()
--- Day30/main.py
+++ Day30/main.py
@@ -1,6 +1,30 @@
-# FileNotFound error
-# with open('file.txt') as file:
-#     file.read()
+# Error handling
+try:
+    file = open("a_file.txt")
+    a_dict = {"key": "value"}
+    print(a_dict['key'])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:  # runs if there are no exceptions
+    content = file.read()
+    print(content)
+finally:  # runs no matter what happens
+    file.close()
+    print("The file was closed.")
+    # raise TypeError("This is an error I made up.")
+
+# Sample raise error - Calculating BMI
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(f"BMI = {bmi:.2f}")
 
 # KeyError
 # a_dict = {"key": "value"}
