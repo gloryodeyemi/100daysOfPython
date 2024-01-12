6f8666e793f9893ec9a753a580d6062879710461
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-12 10:22:36 -0500

    Day 30

---
+++ Day30/main.py
@@ -0,0 +1,15 @@
+# FileNotFound error
+# with open('file.txt') as file:
+#     file.read()
+
+# KeyError
+# a_dict = {"key": "value"}
+# value = a_dict['non_existent_key']
+
+# IndexError
+# fruit_list = ['Apple', 'Banana', 'Pear']
+# fruit = fruit_list[3]
+
+# TypeError
+# text = 'abc'
+# print(text + 5)
