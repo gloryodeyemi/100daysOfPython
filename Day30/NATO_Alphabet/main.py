39d47fdf6d542a73abd6c5cfdd2a70c098d3f52b
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-12 11:37:25 -0500

    Day 30

--- Day30/NATO_Alphabet/main.py
+++ Day30/NATO_Alphabet/main.py
@@ -9,6 +9,7 @@ nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet.ite
 print(f"NATO Alphabet Dictionary:\n-------------------------\n{nato_alphabet_dict}\n")
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+print("\n#----------Option 1----------#")
 user_input = input("Enter a word: ")
 
 while KeyError:
@@ -20,3 +21,19 @@ while KeyError:
     else:
         print(f"NATO code for {user_input}: {nato_list}")
         break
+
+
+# Option 2
+def generate_phonetic():
+    user_input2 = input("Enter a word: ")
+    try:
+        nato_list2 = [nato_alphabet_dict[letter.upper()] for letter in user_input2]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(f"NATO code for {user_input2}: {nato_list2}")
+
+
+print("\n#----------Option 2----------#")
+generate_phonetic()
