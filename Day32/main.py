1b29895b58bb56e6de7b6338d8deba1dfb317298
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-22 09:47:17 -0500

    Day 32

--- Day32/main.py
+++ Day32/main.py
@@ -1,17 +1,18 @@
 import smtplib
+import datetime as dt
 
 my_email = "glowcodes01@gmail.com"
 password = "ykefpbnmrbibdgyb"
 
 # GMAIL
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="glowcodes@yahoo.com",
-        msg="Subject:Hello\n\nThis is the body of my email."
-    )
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="glowcodes@yahoo.com",
+#         msg="Subject:Hello\n\nThis is the body of my email."
+#     )
 
 # YAHOO
 my_yahoo_email = "glowcodes@yahoo.com"
@@ -25,3 +26,16 @@ yahoo_password = "***************"  # currently not available
 #         to_addrs="glowcodes01@gmail.com",
 #         msg="Subject:Hello\n\nThis is the body of my email."
 #     )
+
+# DATETIME
+now = dt.datetime.now()
+print(f"Current datetime: {now}")
+year = now.year
+print(f"Current year: {year}")
+month = now.month
+print(f"Current month: {month}")
+weekday = now.weekday()
+print(f"Current weekday: {weekday}")
+
+date_of_birth = dt.datetime(year=2010, month=8, day=8)
+print(f"My date of birth is: {date_of_birth}")
