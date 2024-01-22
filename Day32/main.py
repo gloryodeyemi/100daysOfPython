5e2d7f0b83e8dc43a56107aa690ee00bf7c21bef
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-22 09:39:09 -0500

    Day 32

--- Day32/main.py
+++ Day32/main.py
@@ -3,12 +3,25 @@ import smtplib
 my_email = "glowcodes01@gmail.com"
 password = "ykefpbnmrbibdgyb"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email,
-                    to_addrs="glowcodes@yahoo.com",
-                    msg="Subject:Hello\n\nThis is the body of my email.")
-connection.close()
+# GMAIL
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="glowcodes@yahoo.com",
+#         msg="Subject:Hello\n\nThis is the body of my email."
+#     )
 
+# YAHOO
+my_yahoo_email = "glowcodes@yahoo.com"
+yahoo_password = "***************"  # currently not available
 
+with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+    connection.starttls()
+    connection.login(user=my_yahoo_email, password=yahoo_password)
+    connection.sendmail(
+        from_addr=my_yahoo_email,
+        to_addrs="glowcodes01@gmail.com",
+        msg="Subject:Hello\n\nThis is the body of my email."
+    )
