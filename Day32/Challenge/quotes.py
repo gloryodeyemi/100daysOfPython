062eb375c9faf849aa8a08b9f8e7bd008e12cc36
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-22 10:03:23 -0500

    Motivational quotes challenge

--- Day32/Challenge/quotes.py
+++ Day32/Challenge/quotes.py
@@ -5,6 +5,27 @@ import random
 # getting the file
 with open("quotes.txt", "r") as quotes_file:
     quotes_list = quotes_file.readlines()
-    print(f"Length of quotes: {len(quotes_list)}\nSample quote: {random.choice(quotes_list)}")
+    random_quote = random.choice(quotes_list)
+    print(f"Length of quotes: {len(quotes_list)}\nSample quote: {random_quote}")
 
-#
+# getting the current weekday
+current_date = dt.datetime.now()
+current_weekday = current_date.weekday()
+print(f"The current weekday is: {current_weekday}")
+
+# sending the email
+my_email = "glowcodes01@gmail.com"
+password = "ykefpbnmrbibdgyb"
+
+# GMAIL
+if current_weekday == 0:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="glowcodes@yahoo.com",
+            msg=f"Subject:Weekly Motivation\n\n{random_quote}"
+        )
+else:
+    print("I am not a monday")
