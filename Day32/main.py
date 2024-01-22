29c0510be2b383e1ae2ce09f993b1fc3da8d0cc7
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-22 09:31:28 -0500

    Day 32

--- Day32/main.py
+++ Day32/main.py
@@ -1,10 +1,14 @@
 import smtplib
 
 my_email = "glowcodes01@gmail.com"
-password = "abc1234"
+password = "ykefpbnmrbibdgyb"
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
 connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="glowcodes@yahoo.com")
+connection.sendmail(from_addr=my_email,
+                    to_addrs="glowcodes@yahoo.com",
+                    msg="Subject:Hello\n\nThis is the body of my email.")
+connection.close()
+
 
