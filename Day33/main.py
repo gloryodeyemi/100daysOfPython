3675bcb33c83941cfd0098119d46a115772b31d6
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-24 09:55:54 -0500

    Day 33

--- Day33/main.py
+++ Day33/main.py
@@ -1,5 +1,12 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response.status_code)
-response.raise_for_status()
+print(response.status_code)  # to get the response status code
+response.raise_for_status()  # to raise an exception is request fails
+data = response.json()  # to get the json data
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(f"ISS position: {iss_position}")
