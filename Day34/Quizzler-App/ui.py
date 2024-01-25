a932871e438151909e744324dd31b7e15ad50bf8
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-01-25 10:22:35 -0500

    Day 34

--- Day34/Quizzler-App/ui.py
+++ Day34/Quizzler-App/ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import  QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.score = 0
         self.window = Tk()
         self.window.title("Quizzler")
@@ -14,7 +16,12 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(bg="white", width=300, height=250, highlightthickness=0)
-        self.quiz_text = self.canvas.create_text(150, 125, text="Quiz here", font=("Arial", 20, "italic"), fill="black")
+        self.quiz_text = self.canvas.create_text(
+            150, 125,
+            text="Quiz here",
+            font=("Arial", 20, "italic"),
+            fill="black",
+            width=280)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         self.false_image = PhotoImage(file="images/false.png")
@@ -25,4 +32,10 @@ class QuizInterface:
         self.true_button = Button(image=self.true_image, bg=THEME_COLOR, highlightthickness=0)
         self.true_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.quiz_text, text=q_text)
