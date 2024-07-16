b1a0b2dad9bd88d7a88715836dddb352a2f28595
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-07-16 17:28:21 -0400

    Web scraping

--- Day45/bs4-start/main.py
+++ Day45/bs4-start/main.py
@@ -2,4 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 
 response = requests.get("https://news.ycombinator.com/news")
-print(response.text)
+
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+
+all_story_links = soup.select("span.titleline a")
+# all_story_links = soup.select(selector=".titleline")
+print(all_story_links)
+
+article_titles = [tag.getText() for tag in all_story_links]
+print(article_titles)
+
+article_links = [tag.get("href") for tag in all_story_links]
+print(article_links)
+
+article_votes = [int(vote.getText()[:-7]) for vote in soup.find_all(name="span", class_="score")]
+print(article_votes)
+
