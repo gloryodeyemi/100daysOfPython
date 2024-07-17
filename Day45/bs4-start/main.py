80f88707d9f85bb4cab357ab6163768d35914709
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-07-17 17:36:12 -0400

    Web scraping

--- Day45/bs4-start/main.py
+++ Day45/bs4-start/main.py
@@ -7,14 +7,18 @@ yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
 
-all_story_links = soup.select("span.titleline a")
-# all_story_links = soup.select(selector=".titleline")
+# all_story_links = soup.select("span.titleline")
+all_story_links = soup.find_all(name="span", class_="titleline")
 print(all_story_links)
+# print("\nTesting")
 
-article_titles = [tag.getText() for tag in all_story_links]
+# for ind in range(len(all_story_links)):
+#     print(all_story_links[ind].find("a"))
+
+article_titles = [tag.find("a").getText() for tag in all_story_links]
 print(article_titles)
 
-article_links = [tag.get("href") for tag in all_story_links]
+article_links = [tag.find("a").get("href") for tag in all_story_links]
 print(article_links)
 
 article_votes = [int(vote.getText()[:-7]) for vote in soup.find_all(name="span", class_="score")]
