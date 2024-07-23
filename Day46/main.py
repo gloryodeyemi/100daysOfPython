1f1ea2386e029bc223f5157b8dee5775b70873e5
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-07-23 09:58:24 -0400

    Day 46

--- Day46/main.py
+++ Day46/main.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
 
 desired_year = input("What year would you like to travel to? (Enter the date using this format YYYY-MM-DD): ")
 
@@ -15,4 +18,15 @@ top_100 = soup.select("li h3#title-of-a-story")
 top_100_titles = [title.getText().replace('\n', '').replace('\t', '') for title in top_100]
 print(top_100_titles)
 
-# Takes top 100 music from a date in the past to create a Spotify playlist.
+# Connect to Spotify
+client_ID = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
