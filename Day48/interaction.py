114fef26a6627dbc2fb6d7c1215e0ebb250eeaab
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-09-16 18:49:20 -0400

    Day 48

--- Day48/interaction.py
+++ Day48/interaction.py
@@ -22,5 +22,20 @@ all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
 search = driver.find_element(By.NAME, "search")
 search.send_keys("Python", Keys.ENTER)
 
+# Challenge 3 - Form Sign up
+driver.get("http://secure-retreat-92358.herokuapp.com/")
+
+first_name = driver.find_element(By.NAME, "fName")
+first_name.send_keys("Glory")
+
+last_name = driver.find_element(By.NAME, "lName")
+last_name.send_keys("Odeyemi")
+
+email = driver.find_element(By.NAME, "email")
+email.send_keys("glowtest@gmail.com")
+
+button = driver.find_element(By.CLASS_NAME, "btn")
+button.click()
+
 
 # driver.quit()
