14bef4b4f5bd1fbfd716b5fe0df08fdb43fcf436
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-09-16 17:32:15 -0400

    Day 48

--- Day48/main.py
+++ Day48/main.py
@@ -1,11 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Keep Chrome browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com")
+# driver.get("https://www.amazon.ca/PanOxyl-Creamy-Acne-Wash-Peroxide/dp/B09NYTQ2KM")
+driver.get("https://www.python.org/")
+
+# Find by class name
+# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+# price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+# print(f"The price is ${price_dollar.text}.{price_cents.text}")
+
+# Find by name
+search_bar = driver.find_element(By.NAME, value='q')
+print(search_bar.tag_name)
+print(search_bar.get_attribute("placeholder"))
+
+# Find by id
+button = driver.find_element(By.ID, value="submit")
+print(button.size)
 
 # driver.close()  # closes a particular tab
 driver.quit()  # quits the entire browser
