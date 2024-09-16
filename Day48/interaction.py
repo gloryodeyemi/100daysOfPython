889d55b9d2aed7b87475f649a26d082b42523620
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-09-16 18:24:03 -0400

    Day 48

---
+++ Day48/interaction.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+# Challenge 2 - My solution
+total_articles = driver.find_element(By.CSS_SELECTOR, value='#articlecount a')
+print(total_articles.text)
+
+
+driver.quit()
