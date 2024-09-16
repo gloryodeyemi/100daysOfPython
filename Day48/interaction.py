5befe612ce42fb69907666ebb293c8a7cea38605
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-09-16 18:41:10 -0400

    Day 48

--- Day48/interaction.py
+++ Day48/interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # Keep Chrome browser open after program finishes
 chrome_options = webdriver.ChromeOptions()
@@ -11,6 +12,15 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 # Challenge 2 - My solution
 total_articles = driver.find_element(By.CSS_SELECTOR, value='#articlecount a')
 print(total_articles.text)
+# total_articles.click()
+
+# Find element by Link Text
+all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
+
+# Sending keyboard input to Selenium
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python", Keys.ENTER)
 
 
-driver.quit()
+# driver.quit()
