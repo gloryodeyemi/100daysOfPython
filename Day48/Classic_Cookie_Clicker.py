33d624aab11cf1b30ff1c028987de73fc012bab0
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-09-18 16:08:19 -0400

    Day 48

--- Day48/Classic_Cookie_Clicker.py
+++ Day48/Classic_Cookie_Clicker.py
@@ -20,9 +20,8 @@ def convert_str_to_int(element):
 
 
 def get_store_cost():
-    store = driver.find_element(By.ID, value="store")
-    store = store.text.split("\n")
-    store_costs = [convert_str_to_int(item.split('-')[1].strip()) for item in store if '-' in item]
+    store = driver.find_elements(by=By.CSS_SELECTOR, value="#store div b")
+    store_costs = [convert_str_to_int(item.text.split('-')[1].strip()) if '-' in item.text else 0 for item in store]
     store_costs.reverse()
     return store_costs
 
@@ -38,19 +37,11 @@ def get_store_item_ids():
     return item_ids
 
 
-# store_item_ids = [
-#     'buyTime machine',
-#     'buyPortal',
-#     'buyAlchemy lab',
-#     'buyShipment',
-#     'buyMine',
-#     'buyFactory',
-#     'buyGrandma',
-#     'buyCursor'
-# ]
-click_interval = 0.01
+# click_interval = 0.01
 purchase_check_interval = 5
 game_duration = 300  # 5 minutes = 300 seconds
+store_item_ids = get_store_item_ids()  # Get the ids of the store items
+print(store_item_ids)
 
 # Continuous clicking loop
 try:
@@ -74,11 +65,11 @@ try:
             cookies = convert_str_to_int(driver.find_element(By.ID, value="money").text)
             print(f"Cookies available: {cookies}")
             item_costs = get_store_cost()  # Get the current costs of the store items
-            store_item_ids = get_store_item_ids()  # Get the ids of the store items
+            print(f"Cost of items: {item_costs}")
 
             # Find the affordable store items to purchase
             for ind in range(len(store_item_ids)):
-                if cookies >= item_costs[ind]:
+                if cookies >= item_costs[ind] != 0:
                     item_to_buy = driver.find_element(By.ID, value=f"{store_item_ids[ind]}")
                     item_to_buy.click()
                     print(f"Purchased item: {store_item_ids[ind]}")
---
+++ Day48/misc.py
@@ -0,0 +1,17 @@
+# def get_store_cost():
+#     store = driver.find_element(By.ID, value="store")
+#     store = store.text.split("\n")
+#     store_costs = [convert_str_to_int(item.split('-')[1].strip()) for item in store if '-' in item]
+#     store_costs.reverse()
+#     return store_costs
+
+# store_item_ids = [
+#     'buyTime machine',
+#     'buyPortal',
+#     'buyAlchemy lab',
+#     'buyShipment',
+#     'buyMine',
+#     'buyFactory',
+#     'buyGrandma',
+#     'buyCursor'
+# ]
